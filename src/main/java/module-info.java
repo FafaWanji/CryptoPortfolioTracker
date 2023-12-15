bc853b4473612a4ca4a130a1d4fb4be82d76d276
FafaWanji <134288753+FafaWanji@users.noreply.github.com>
2023-12-15 13:19:59 +0100

    init

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module de.fafawanji.cryptoportfoliotracker {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens de.fafawanji.cryptoportfoliotracker to javafx.fxml;
+    exports de.fafawanji.cryptoportfoliotracker;
+}
